f5294df316f7ad1f11b073d4300add2b0c8f0b9c
2367581316 <69404916+2367581316@users.noreply.github.com>
2024-11-21 07:14:59 +0800

    Add files via upload

---
+++ onlyto/js/skystar.js
@@ -0,0 +1,107 @@
+// poem
+var words=[
+    '姜妍',
+    '姜妍',
+    '姜妍',
+    '姜妍',
+    '姜妍',
+    '姜妍',
+    '画船听雨眠',
+    '愿为江水',
+    '与君重逢',
+    '一日不见兮',
+    '思之若狂',
+     '很享受跟你在一起的感觉',
+    '喜欢看你对我撒娇的样子',
+    '喜欢你对我笑的样子',
+    '你是我灰色人生中的一道彩虹',
+    '很庆幸在我枯燥的大学生活中遇到你',
+    '你的陪伴是我最开心的日子',
+    '你对我说的话我都会记在心上',
+    '你开心的时候也是我最开心的时刻',
+    '谢谢遇到了你小姜同学',
+    '今夜何夕',
+    '见此良人',
+    '愿我如星君如月',
+    '夜夜流光相皎洁',
+    '情不所起',
+    '一往而深',
+    '玲珑骰子安红豆',
+    '入骨相思知不知',
+    '多情只有春庭月',
+    '尤为离人照落花',
+    '若有知音见采',
+    '不辞唱遍阳春',
+    '休言半纸无多重',
+    '万斛离愁尽耐担',
+    '夜月一帘幽梦',
+    '醉后不知天在水',
+    '满船星梦压星河',
+    '落花人独立',
+    '微雨燕双飞',
+    '掬水月在手',
+    '弄花香满衣',
+    '夜深忽梦少年事',
+    '唯梦闲人不梦君',
+    '垆边人似月',
+    '皓腕凝霜雪',
+    '众里嫣然通一顾',
+    '人间颜色如尘土',
+    '若非群玉山头见',
+    '会向瑶台月下逢',
+    '沉鱼落雁鸟惊喧',
+    '羞花闭月花愁颤',
+    '解释春风无限恨',
+    '沉香亭北倚阑干'
+];
+function randomNum(min,max){
+    var num = (Math.random()*(max-min+1)+min).toFixed(2);
+    return num;
+}
+function init(){
+    let container = document.querySelector('.container');
+    let f = document.createDocumentFragment();
+    words.forEach(w=>{
+    let word_box = document.createElement('div');
+    let word = document.createElement('div');
+        word.innerText = w;
+        word.classList.add('word');
+        word.style.color = '#BAABDA';
+        word.style.fontFamily = '楷体';
+        word.style.fontSize = '20px'
+        word_box.classList.add('word-box');
+        word_box.style.setProperty("--margin-top",randomNum(-40,20)+'vh');
+        word_box.style.setProperty("--margin-left",randomNum(6,35)+'vw');
+        word_box.style.setProperty("--animation-duration",randomNum(8,20)+'s');
+        word_box.style.setProperty("--animation-delay",randomNum(-20,0)+'s');
+        
+        word_box.appendChild(word);
+        f.appendChild(word_box);
+
+
+    })
+    container.appendChild(f);
+}
+window.addEventListener('load',init);
+let textone = document.querySelector('.textone').querySelector('h1');
+      let texttwo = document.querySelector('.texttwo').querySelector('h1');
+      let textthree = document.querySelector('.textthree').querySelector('h1');
+
+      setTimeout(function(){
+        textone.innerHTML = '把我认为最浪漫的东西送给我最喜欢的 小姜同学';
+          textone.style.color = '#E8F9FD';
+          textone.style.fontFamily = '楷体'
+          texttwo.style.color = '#E8F9FD';
+          texttwo.style.fontFamily = '楷体'
+          textthree.style.color = '#E8F9FD';
+          textthree.style.fontFamily = '楷体'
+          texttwo.innerHTML = '';
+      },4000)
+      setTimeout(function(){
+        textone.innerHTML = '星空是未知神秘且浪漫的';
+        texttwo.innerHTML = '但又如此的令人向往';
+        textthree.innerHTML = '我对你的感情也是如此';
+      },11250)
+
+
+ 
